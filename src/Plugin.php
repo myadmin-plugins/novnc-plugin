015932f719a72e4044d5d4a2e6550472c411c372
Joe Huss <detain@interserver.net>
2017-09-01 17:37:39 -0400

    link updates replacing hardcoded hostname with relative path

--- src/Plugin.php
+++ src/Plugin.php
@@ -40,7 +40,7 @@ class Plugin {
 		if ($GLOBALS['tf']->ima == 'admin') {
 			function_requirements('has_acl');
 					if (has_acl('client_billing'))
-							$menu->add_link('admin', 'choice=none.abuse_admin', '//my.interserver.net/bower_components/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', 'Novnc');
+							$menu->add_link('admin', 'choice=none.abuse_admin', '/bower_components/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', 'Novnc');
 		}
 	}
 
