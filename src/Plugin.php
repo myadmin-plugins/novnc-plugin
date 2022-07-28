80d7efc353d64cde0f1ff41dc6da9b2a6165497e
Joe Huss <detain@interserver.net>
2022-07-28 17:05:40 -0400

    formatting update

--- src/Plugin.php
+++ src/Plugin.php
@@ -11,65 +11,65 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  */
 class Plugin
 {
-	public static $name = 'Novnc Plugin';
-	public static $description = 'Allows handling of NoVNC HTML5 VNC Connections';
-	public static $help = '';
-	public static $type = 'plugin';
+    public static $name = 'Novnc Plugin';
+    public static $description = 'Allows handling of NoVNC HTML5 VNC Connections';
+    public static $help = '';
+    public static $type = 'plugin';
 
-	/**
-	 * Plugin constructor.
-	 */
-	public function __construct()
-	{
-	}
+    /**
+     * Plugin constructor.
+     */
+    public function __construct()
+    {
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function getHooks()
-	{
-		return [
-			//'system.settings' => [__CLASS__, 'getSettings'],
-			//'ui.menu' => [__CLASS__, 'getMenu'],
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function getHooks()
+    {
+        return [
+            //'system.settings' => [__CLASS__, 'getSettings'],
+            //'ui.menu' => [__CLASS__, 'getMenu'],
+        ];
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getMenu(GenericEvent $event)
-	{
-		$menu = $event->getSubject();
-		if ($GLOBALS['tf']->ima == 'admin') {
-			function_requirements('has_acl');
-			if (has_acl('client_billing')) {
-			}
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getMenu(GenericEvent $event)
+    {
+        $menu = $event->getSubject();
+        if ($GLOBALS['tf']->ima == 'admin') {
+            function_requirements('has_acl');
+            if (has_acl('client_billing')) {
+            }
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getRequirements(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Plugins\Loader $this->loader
-		 */
-		$loader = $event->getSubject();
-		$loader->add_requirement('class.Novnc', '/../vendor/detain/myadmin-novnc-plugin/src/Novnc.php');
-		$loader->add_requirement('deactivate_kcare', '/../vendor/detain/myadmin-novnc-plugin/src/abuse.inc.php');
-		$loader->add_requirement('deactivate_abuse', '/../vendor/detain/myadmin-novnc-plugin/src/abuse.inc.php');
-		$loader->add_requirement('get_abuse_licenses', '/../vendor/detain/myadmin-novnc-plugin/src/abuse.inc.php');
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getRequirements(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
+        $loader->add_requirement('class.Novnc', '/../vendor/detain/myadmin-novnc-plugin/src/Novnc.php');
+        $loader->add_requirement('deactivate_kcare', '/../vendor/detain/myadmin-novnc-plugin/src/abuse.inc.php');
+        $loader->add_requirement('deactivate_abuse', '/../vendor/detain/myadmin-novnc-plugin/src/abuse.inc.php');
+        $loader->add_requirement('get_abuse_licenses', '/../vendor/detain/myadmin-novnc-plugin/src/abuse.inc.php');
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Settings $settings
-		 **/
-		$settings = $event->getSubject();
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+    }
 }
